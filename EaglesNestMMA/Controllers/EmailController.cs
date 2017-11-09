6d6973d05b11f3fbf0b178cfbc0428ee813b4a77
Luke Harris <luke.e.harris@gmail.com>
2017-11-09 14:07:22 -0600

    Created 'email' controller and wrote methods to send email from contact us form

---
+++ EaglesNestMMA/Controllers/EmailController.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Mail;
+using System.Threading.Tasks;
+using System.Web;
+using System.Web.Mvc;
+
+namespace EaglesNestMMA.Controllers
+{
+    public class EmailController : Controller
+    {
+        // GET: Email
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        // Called when user clicks the submit button and form values are passed into it
+        public async Task<ActionResult> SendContactUsEmail(FormCollection form)
+        {
+            var name = form["sname"];
+            var email = form["semail"];
+            var messages = form["smessage"];
+            var phone = form["sphone"];
+            var x = await EmailStatus(name, email, messages, phone);
+            if (x == "sent")
+                ViewData["esent"] = "Your Message Has Been Sent";
+            return RedirectToAction("Index", "Home");
+        }
+
+
+        private async Task<string> EmailStatus(string name, string email, string messages, string phone)
+        {
+            var message = new MailMessage();
+            message.To.Add(new MailAddress("etest1482@gmail.com"));    
+            message.From = new MailAddress("etest1482@gmail.com");   
+            message.Subject = "Message From" + email;
+            message.Body = "Name: " + name + "\nFrom: " + email + "\nPhone: " + phone + "\nMessage: " + messages;
+            message.IsBodyHtml = true;
+            using (var smtp = new SmtpClient())
+            {
+                var credential = new NetworkCredential
+                {
+                    UserName = "etest1482@gmail.com",  
+                    Password = "Password1482%"   
+                };
+
+                // email server settings
+                smtp.Credentials = credential;
+                smtp.Host = "smtp.gmail.com";
+                smtp.Port = 587;
+                smtp.EnableSsl = true;
+                await smtp.SendMailAsync(message);
+
+                return "sent";
+            }
+        }
+    }
+}
